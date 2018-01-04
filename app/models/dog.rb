cb9349b30f312733aee1a2ea9262204a863b3143
Hoon Lee <yl.hoony@gmail.com>
2018-01-04 07:21:14 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,10 +4,10 @@ class Dog
 
   @@all = []
 
-  def initialize(name:, breed:, age:)
-    @name = name
-    @breed = breed
-    @age = age
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send("#{key}=", value)
+    end
   end
 
   def save
